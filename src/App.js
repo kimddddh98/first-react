450c8e91eb276c1b600e2202f0035295c1a4e252
kimddddh98 <amawang11@gmail.com>
2023-02-12 21:09:17 +0900

    test

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import src from './logo.svg'
 function App() {
   const arr = [
     {title:"js"},
-    {title:"vue"},
+    {title:"vue"}, 
     {title:"react"},
   ]
   return (
     <div className="App">
     <HeaderTest src={src}/>
     <ComponentTest title="react" arr={arr}/>
+
     </div>
   );
 };
 function ComponentTest(props){
-  const list = props.arr.map((item, index) =>(<li key={index}>{item.title}</li>))
+  const list = props.arr.map((item, index) =>(<li keyq={index}>{item.title}</li>))
 
   return <ul>
     <li>{list}</li>
